dd8c03c188bd5c6d382b413279202cd250bb46a1
aliaksandrnaronski <aliaksandr.naronski@itechart-group.com>
2025-02-05 20:20:11 +0300

    actions3

--- config/wdio.shared.conf.js
+++ config/wdio.shared.conf.js
@@ -1,29 +1,17 @@
-
-export const config = {
-
+module.exports.config = {
     specs: [],
-
     capabilities: [],
-
     logLevel: 'debug',
-
     bail: 0,
-
     baseUrl: 'http://the-internet.herokuapp.com',
-
     waitforTimeout: 45000,
-
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-
     services: [],
-
     framework: 'mocha',
-
     reporters: ['spec'],
     mochaOpts: {
         ui: 'bdd',
-
         timeout: 3 * 60 * 1000,
     },
 };
